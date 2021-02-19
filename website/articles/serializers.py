e3039362b6403b22b8122f2cdfb89a3c68dc5fcb
Nicholas Juncos <nicholas.juncos@agfc.ar.gov>
2021-02-19 14:51:24 -0600

    Created permissions for articles. Created admin serializer. Customized ArticleViewSet to check for language filter, and return a message if articles are empty.

--- website/articles/serializers.py
+++ website/articles/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from .models import Article
+from ..common import PREFERRED_LANGUAGE_CHOICES
 
 
 class ArticleSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    language = serializers.ChoiceField(choices=PREFERRED_LANGUAGE_CHOICES, source='get_language_display')
 
     class Meta:
         model = Article
@@ -18,3 +20,17 @@ class ArticleSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['author'] = self.context['request'].user
         return super().create(validated_data)
+
+
+class AdminArticleSerializer(serializers.ModelSerializer):
+    author_name = serializers.ReadOnlyField(source='author.username')
+    language = serializers.ChoiceField(choices=PREFERRED_LANGUAGE_CHOICES, source='get_language_display')
+
+    class Meta:
+        model = Article
+        fields = ['status', 'author', 'author_name', 'title', 'post', 'language', 'publication_date', 'url']
+        read_only_fields = ('publication_date',)
+
+        extra_kwargs = {
+            'url': {'view_name': 'api:article-detail', 'lookup_field': 'id'}
+        }
--- website/articles/views.py
+++ website/articles/views.py
@@ -1,10 +1,12 @@
 from django.db.models import Q
-from rest_framework import filters, permissions
+from rest_framework import filters, status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Article
-from .serializers import ArticleSerializer
+from .permissions import IsAuthorOrReadOnly
+from .serializers import ArticleSerializer, AdminArticleSerializer
 
 
 class ArticleViewSet(ModelViewSet):
@@ -14,8 +16,13 @@ class ArticleViewSet(ModelViewSet):
 
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     filterset_fields = ['title', 'post', 'author__username', 'language', ]
-    search_fields = ['title', 'post', 'author__username', 'language', ]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    search_fields = ['title', 'post', 'author__username', ]
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.request.user.is_superuser:
+            return AdminArticleSerializer
+        return self.serializer_class
 
     def get_queryset(self, *args, **kwargs):
         # allow superusers to see all articles
@@ -24,4 +31,30 @@ class ArticleViewSet(ModelViewSet):
         # by default only show articles by users themselves and any published articles
         if not self.request.user.is_authenticated:
             return self.queryset.filter(status='Published')
-        return self.queryset.filter(Q(author_id=self.request.user.id) | Q(status='Published'))
+        # getting language from url arguments. If none, then queryset is based on preferred language
+        language = self.request.GET.get('language')
+        if language:
+            return self.queryset.filter(Q(author_id=self.request.user.id) | Q(status='Published'))
+        return self.queryset.filter(Q(author_id=self.request.user.id) |
+                                    Q(status='Published', language=self.request.user.preferred_language))
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        # below detects if a language is filtered. If not, and the queryset is empty, returns a message
+        language = self.request.GET.get('language')
+        if not language:
+            message = 'There are no articles for your preferred language! Please add a filter for another language to' \
+                      ' possibly view other articles.'
+        else:
+            message = 'There are no articles! Please add/edit language filter to possibly view other articles.'
+        if not any(serializer.data):
+            headers = self.get_success_headers(serializer.data)
+            return Response({'Message': message}, status=status.HTTP_204_NO_CONTENT, headers=headers)
+        return Response(serializer.data)
